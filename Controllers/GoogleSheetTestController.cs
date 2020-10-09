215e90d4cb17396021798535fe6e94a705600717
mrmxs <maryzmaxs@gmail.com>
2020-10-09 19:32:22 +0300

    Add Place DTO. Edit controller to get, get-by-id and post Place-data

--- Controllers/GoogleSheetTestController.cs
+++ Controllers/GoogleSheetTestController.cs
@@ -13,8 +13,8 @@ using Google.Apis.Sheets.v4.Data;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
 
+using Korobochka.DTOs;
 using Korobochka.Models;
-using Korobochka.Repositories; // TODO replace using of googleSheetsSettings to repositories
 using Korobochka.Services;
 
 namespace Korobochka.Controllers
@@ -23,87 +23,68 @@ namespace Korobochka.Controllers
     [Route("[controller]")]
     public class GoogleSheetTestController : ControllerBase
     {
-        // If modifying these scopes, delete your previously saved credentials
-        // at ~/.credentials/sheets.googleapis.com-dotnet-quickstart.json
-        static string _applicationName = "Korobochka";
-        static string[] _scopes = { SheetsService.Scope.SpreadsheetsReadonly };
-        // The Web API will only accept tokens 1) for users, and 2) having the "access_as_user" scope for this API
-        // static readonly string[] _scopeRequiredByApi = new string[] { "access_as_user" };
-        private readonly IGoogleSheetsSettings _gSheetsSettings;
-
         private readonly ILogger<GoogleSheetTestController> _logger;
-
         private PlacesService _placesService;
 
         public GoogleSheetTestController(
-            IGoogleSheetsSettings gSheetsSettings,
+            // IGoogleSheetsSettings gSheetsSettings,
             ILogger<GoogleSheetTestController> logger,
             PlacesService placesService)
         {
             _logger = logger;
-            _gSheetsSettings = gSheetsSettings;
+            // _gSheetsSettings = gSheetsSettings;
             _placesService = placesService;
         }
 
         [HttpGet]
-        public IEnumerable<string> Get()
+        public /* TODO override*/ ActionResult<IEnumerable<PlaceDTO>> Get()
         {
-
-            UserCredential credential;
-
-            using (var stream =
-                new FileStream("token.json/client_secret.json", FileMode.Open, FileAccess.Read))
+            try
             {
-                // The file token.json stores the user's access and refresh tokens, and is created
-                // automatically when the authorization flow completes for the first time.
-                string credPath = "token.json";
-                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-                    GoogleClientSecrets.Load(stream).Secrets,
-                    _scopes,
-                    "user",
-                    CancellationToken.None,
-                    new FileDataStore(credPath, true)).Result;
-                Console.WriteLine("Credential file saved to: " + credPath);
+                // // TODO if null sequence
+                return Ok(_placesService.Get().Select(item => (PlaceDTO)item));
             }
-
-            // Create Google Sheets API service.
-            var service = new SheetsService(new BaseClientService.Initializer()
+            catch (Exception e)
             {
-                HttpClientInitializer = credential,
-                ApplicationName = _applicationName,
-            });
-
-            // Define request parameters.
-            // String spreadsheetId = "1Fd09448owbmW9DLGbcd9qC_KUoyGux3zHFHoAps9WS0"; // <-- Korobochka
-            // String range = "Places!A2:G";
-            SpreadsheetsResource.ValuesResource.GetRequest request =
-                    // service.Spreadsheets.Values.Get(spreadsheetId, range);
-                    service.Spreadsheets.Values.Get(
-                        _gSheetsSettings.SpreadsheetId, _gSheetsSettings.PlacesRange);
-
-            // Prints the names and majors of students in a sample spreadsheet:
-            // https://docs.google.com/spreadsheets/d/1Fd09448owbmW9DLGbcd9qC_KUoyGux3zHFHoAps9WS0/edit
-            ValueRange response = request.Execute();
-            IList<IList<Object>> values = response.Values;
-            if (values != null && values.Count > 0)
-            {
-                var result = new List<string>();
-                foreach (var row in values)
-                {
-                    // Print columns A and E, which correspond to indices 0 and 4.
-                    // Console.WriteLine("{0}\t{1}", row[1], row[1]);
-                    result.Add($"#{row[0]}({row[6]})\t{row[1]}");
-                }
-                return result;
+                return CustomBadRequest(e.Message);
             }
-            return new string[] { "No ", "data ", "found." };
-
         }
 
-        [HttpPut]
-        public IEnumerable<Place> Put()
+        [HttpGet("{id}")]
+        public /*override*/ ActionResult<PlaceDTO?> Get(int id)
         {
-            return _placesService.Get();
+            try
+            {
+                var result = _placesService.Get(id);
+
+                if (result != null) return Ok((PlaceDTO)result);
+                return Ok(null);
+            }
+            catch (Exception e)
+            {
+                return CustomBadRequest(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public /*override*/ ActionResult<PlaceDTO> Post([FromBody] PlaceDTO value)
+        {
+            try
+            {
+                // // TODO if wrong data
+                return Ok((PlaceDTO)(_placesService.Create((Place)value))); //spreadsheetinfo - range
+
+            }
+            catch (Exception e)
+            {
+                return CustomBadRequest(e.Message);
+            }
+        }
+
+        //TODO to BaseController
+        protected BadRequestObjectResult CustomBadRequest(object error)
+        {
+            return BadRequest(new { Error = error });
         }
     }
 }
